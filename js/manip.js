692290cd339f6bf24db10e4a4dd02a364b9fec34
KABANOFESTO <kabanofesto1@gmail.com>
2024-01-12 05:44:18 -0800

    ch-homeRes

--- js/manip.js
+++ js/manip.js
@@ -12,16 +12,43 @@
 //         about.style.display="none";
 //     }
 // }
-function hidePage(targetSectionId) {
-    const sections = ["about-section", "skill-section"];
+// function hidePage(targetSectionId) {
+//     const sections = ["about-section", "skill-section"];
 
-    for (const sectionId of sections) {
-        const section = document.getElementById(sectionId);
+//     for (const sectionId of sections) {
+//         const section = document.getElementById(sectionId);
 
-        if (sectionId === targetSectionId) {
-            section.style.display = section.style.display === "none" || section.style.display === "" ? "block" : "none";
+//         if (sectionId === targetSectionId) {
+//             section.style.display = section.style.display === "none" || section.style.display === "" ? "block" : "none";
+//         } else {
+//             section.style.display = "none";
+//         }
+//     }
+// }
+const reveal = () => {
+    const reveals = document.querySelectorAll('.reveal');
+
+    for (let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
         } else {
-            section.style.display = "none";
+            reveals[i].classList.remove('active');
         }
     }
-}
+};
+window.addEventListener('scroll', reveal);
+
+const activePage = window.location.pathname;
+
+const navLinks = document.querySelector('nav a');
+
+navLinks.forEach(link => {
+    if (link.href.includes(`${activePage}`)) {
+        link.classList.add('active');
+    }
+});
+
